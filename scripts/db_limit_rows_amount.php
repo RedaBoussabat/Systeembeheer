b37f71b861174b106aaf2660fc1c5c9fa354ea01
reda.boussabat@student.ucll.be <reda.boussabat@student.ucll.be>
2020-11-20 17:59:18 +0100

    DB limit rows script

---
+++ scripts/db_limit_rows_amount.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "DragonKid";
+$dbname = "check";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+}
+//     // Query
+mysqli_query($conn, "DELETE FROM log WHERE date NOT IN ( SELECT date FROM ( SELECT date FROM log ORDER BY date DESC LIMIT 90 ) sectable );");
+$conn->close();
+?>
